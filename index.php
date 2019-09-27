1aa433514cf3c54de5a0be12c88f1ae6e1858938
thiagoirch <thiagoirch@gmail.com>
2019-09-27 11:33:32 -0300

    PHP v2

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?
+<?php
+// start output buffering
+ob_start();
+
+// send phpinfo content
 phpinfo();
-?>
+
+// get phpinfo content
+$html = ob_get_contents();
+
+// flush the output buffer
+ob_end_clean();
+
+// remove auth data
+if (isset($_SERVER['AUTH_USER'])) $html = str_replace($_SERVER['AUTH_USER'], '<i>no value</i>', $html);
+if (isset($_SERVER['AUTH_PASSWORD'])) $html = str_replace($_SERVER['AUTH_PASSWORD'], '<i>no value</i>', $html);
+
+echo $html;
